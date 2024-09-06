3a6a1b7a0b27c5eae5b496b6b175acf773da6160
hrmrakib <hrmrakibs@gmail.com>
2024-09-06 15:43:44 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Ques-1: Palindrome number
+// An integer is a palindrome when it reads the same backward as forward.
+
+//  Input: 121 -----> Output: true
+//  Input: -121 -----> Output: false
+//  Input: 10 -----> Output: false
+
+const isPalindrome = (x) => {};
+
+// Ques-2: Armstrong number
+
+// Ques-3: Prime number
+
+// Ques-4: Fibonacci series
+
+// Ques-5: Armstrong number in given range
+
+// Ques-6: Prime number in given range
+
+// Ques-7: Armstrong number in given range
+
+// Ques-8: Prime number in given range
